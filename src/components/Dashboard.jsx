dc059209576418cbe21a856b4cb70d8c39082a8e
zishan chaudhary <zishan8291@gmail.com>
2024-04-01 16:55:34 +0530

    improve logout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { firstName, lastName, isLogin } = location.state || {};
+
+  const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
 
   useEffect(() => {
     if (!isLogin) {
@@ -13,7 +15,19 @@ const Dashboard = () => {
   }, [isLogin, navigate]);
 
   const handleLogout = () => {
+    // Display confirmation dialog
+    setShowLogoutConfirmation(true);
+  };
+
+  const confirmLogout = () => {
+    // Clear session data, local storage, etc.
+    // Redirect to the login page
     navigate("/");
+  };
+
+  const cancelLogout = () => {
+    // Hide confirmation dialog
+    setShowLogoutConfirmation(false);
   };
 
   return (
@@ -35,6 +49,29 @@ const Dashboard = () => {
           Welcome
         </h1>
       </main>
+      {showLogoutConfirmation && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <p className="text-lg text-gray-800 mb-4">
+              Are you sure you want to log out?
+            </p>
+            <div className="flex justify-end">
+              <button
+                onClick={cancelLogout}
+                className="px-4 py-2 mr-2 bg-gray-400 text-gray-800 rounded hover:bg-gray-500 hover:text-white focus:outline-none"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={confirmLogout}
+                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none"
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
